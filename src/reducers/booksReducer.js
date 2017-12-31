08cbe3e345ad2127646f3d0c956b66290e316262
zgreathouse <greathouse.zachary@gmail.com>
2017-12-30 18:52:42 -0800

    fix syntax error in booksReducer file

--- src/reducers/booksReducer.js
+++ src/reducers/booksReducer.js
@@ -1,7 +1,7 @@
 export default function() {
   return [
-    {title: 'CowboyBebop'}
-    {title: 'Samurai Champloo'}
+    {title: 'CowboyBebop'},
+    {title: 'Samurai Champloo'},
     {title: 'Full Metal Alchemist: Brotherhood'}
   ]
 }
